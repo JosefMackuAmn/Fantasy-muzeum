14ee540114c43edbf1e8bac1bf3ab242cf6b7f7b
Josef Macků <josef@macku.cz>
2020-08-19 17:21:24 +0200

    First commit to branch patrik

---
+++ js/script.js
@@ -0,0 +1,31 @@
+///// DEFINING FUNCTIONS
+// jQuery .ready() fcn equivalent
+const ready = callback => {
+    if (document.readyState !== 'loading') {
+        callback();
+    } else if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', callback);
+    } else {
+        document.attachEvent('onreadystatechange', function() {
+            if (document.readyState === 'complete') {
+                callback();
+            }
+        })
+    }
+}
+
+// Calling ready function
+ready(() => {
+    ///// ANIMATIONS
+    // Move head on index page on scroll
+    const movableHead = document.getElementById('movable-head');
+    if (movableHead) {
+        window.addEventListener('scroll', () => {            
+            if (movableHead) {
+                let scrollTop = window.pageYOffset;
+                let moveHeadInRem = ((scrollTop / 100) * 1.2) - 15;
+                movableHead.style.left = moveHeadInRem + 'rem';
+            }
+        });
+    }
+});
